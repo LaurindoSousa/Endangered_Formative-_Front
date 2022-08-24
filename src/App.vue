c6c5fd6013c73ee12ae92b6128239d00f0cd70a3
Isha Khopkar <ishakhopkar@ymail.com>
2022-08-24 13:56:16 +1200

    add fetching and response for GET animals

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,28 @@
 
 </div>
 
-
+ <button @click="getAnimals" type="button">Get Animals</button>
 
 </template>
 
 <script>
   export default {
-    
+    components: {
+
+    },
+    data() {
+
+    },
+    methods: {
+      async getAnimals() {
+        //console.log('hello');
+        const response = await fetch ('http://localhost:3000/animals');
+        const data = await response.json()
+        //console.log(data);
+        this.animalsArray = data;
+
+      },
+    }
   }
 </script>
 
