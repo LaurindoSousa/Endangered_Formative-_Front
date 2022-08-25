f4073785df442cc221a6ee8bf04595326754edf3
Isha Khopkar <ishakhopkar@ymail.com>
2022-08-25 14:01:17 +1200

    GET array fetched

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,38 @@
 
 </div>
 
-
+ <button @click="getAnimals" type="button">Get Animals</button>
 
 </template>
 
 <script>
   export default {
-    
+    components: {
+
+    },
+    data() {
+
+      return{
+        name:"",
+        scientific:"",
+        status:0,
+        population:"",
+        habitat:"",
+        animalsArray:[]
+      }
+
+    },
+    methods: {
+      async getAnimals() {
+        // console.log('hello');
+        const response = await fetch ('http://localhost:3000/animals');
+        const data = await response.json()
+        //console.log(data);
+        this.animalsArray = data;
+       
+
+      },
+    }
   }
 </script>
 
