6e22d2d966ebc8dda18a04e90ab1588115251ac1
mooloo2214 <mooloo2214@github.com>
2022-08-25 14:31:15 +1200

    Header, banner and categories button added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-<div>
+
+<header>
+  <div id="header-signin">Sign in</div>
+<div id="header-donate"><a href="https://support.worldwildlife.org/site/SPageServer?pagename=main_monthly&s_src=AWE2212OQ18299A06206CX&s_subsrc=topnav&_ga=2.14339250.486991054.1661133227-873731236.1660792952">Donate</a></div>
+</header>
+
+
+<Banner/>
+
+
+<div class="menu-container">
+  <div id="menu-categories">
+    <h4>Search by</h4>
+<ul>
+  
+  <li><button @click="getAnimals" type="button">All</button></li>
+  <li><button>Critically Endangered</button></li>
+  <li><button>Endangered</button></li>
+  <li><button>Vulnerable</button></li>
+  <li><button>Near Threatened</button></li>
+</ul>
+
+  </div>
 
 </div>
-
- <button @click="getAnimals" type="button">Get Animals</button>
 
 </template>
 
