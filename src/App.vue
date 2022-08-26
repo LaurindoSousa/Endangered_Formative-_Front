5e36b14ee59c48275e83875ee36f06a4f23769b0
Laurindo Sousa <laurindosousa@ymail.com>
2022-08-26 15:14:38 +1200

    modified app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
 <div>
   <h1>Hello World</h1>
-  <button type="button">Delete</button>
+  <button @click="deleteAnimals(0)" type="button">Delete</button>
 </div>
 
  <button @click="getAnimals" type="button">Get Animals</button>
+ {{animalsArray}}
 
 </template>
 
@@ -13,17 +14,31 @@
 
     data(){
       return{
+        name:"",
+        scientific:"",
+        status:0,
+        population:"",
+        habitat:"",
         animalsArray:[]
 
       }
     },
+
     methods: {
+      async getAnimals() {
+        // console.log('hello');
+        const response = await fetch ('http://localhost:3000/animals');
+        const data = await response.json()
+        console.log(data);
+        this.animalsArray = data;
+      },
+        
 
       async deleteAnimals(id) {
         
         const response = await fetch(`http://localhost:3000/animals/${id}`, {
-          method: 'DELETE',
-        });
+        method: 'DELETE',
+        })
         
         const data = await response.json();
        
@@ -31,10 +46,10 @@
   
         this.animalsArray = data;
       }
-    }
+       
 
-    
-  };
+      }
+    }
 
 </script>
 
